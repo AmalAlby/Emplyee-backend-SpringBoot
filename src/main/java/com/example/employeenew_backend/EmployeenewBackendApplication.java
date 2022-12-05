f062bca5876efc0f6fd6c3fdf50aee886cd83b29
AmalAlby <amalalbyn@gmail.com>
2022-12-05 15:32:04 +0530

    Employee backend basic steps added

---
+++ src/main/java/com/example/employeenew_backend/EmployeenewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeenew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeenewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeenewBackendApplication.class, args);
+	}
+
+}
